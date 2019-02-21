deaa5f7900dfeaa8d1a2ae574a2dff6c6ba16a69
Nick Ball <nick.m.ball1@gmail.com>
2019-02-20 19:27:53 -0800

    Add files via upload

---
+++ Timer/Timer/Form1.vb
@@ -0,0 +1,15 @@
+﻿Public Class Form1
+    Dim counter As Integer
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        counter = counter + 1
+        Label1.Text = counter
+        If counter = 200 Then
+            Timer1.Enabled = False
+
+        End If
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Timer1.Enabled = True
+    End Sub
+End Class
---
+++ Variable/Variable/Form1.vb
@@ -0,0 +1,14 @@
+﻿Public Class Form1
+    Dim x As Double
+    Dim Mytext As String
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        x = TxtNumber.Text
+        Label2.Text = ("The number is ") & x
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Mytext = TxtWords.Text
+        Label4.Text = Mytext
+    End Sub
+End Class
