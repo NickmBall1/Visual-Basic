d05b991513bd9de73641ae3626a67555555ddb3a
Nick Ball <nick.m.ball1@gmail.com>
2019-02-20 18:53:48 -0800

    Add files via upload

---
+++ MagicEightBall/MagicEightBall/Form1.vb
@@ -0,0 +1,20 @@
+﻿Public Class Form1
+    Dim rnd As New Random
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim newnumber As Integer
+        newnumber = rnd.Next(1, 6)
+        Select Case newnumber
+            Case 1
+                MessageBox.Show("Arrays are awesome")
+            Case 2
+                MessageBox.Show("Why aren't you using a Array")
+            Case 3
+                MessageBox.Show("Arrays are better than select cases")
+            Case 4
+                MessageBox.Show("Arrays are a list function")
+            Case 5
+                MessageBox.Show("This is getting repetitive")
+        End Select
+
+    End Sub
+End Class
